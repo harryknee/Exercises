bf4f60f61a1d182231ec0da75ba81cea5d42e843
Shizune <57296495+harryknee@users.noreply.github.com>
2021-10-21 22:44:18 +0000

    improved code

--- 3.2 For Loop/factorial.hpp
+++ 3.2 For Loop/factorial.hpp
@@ -2,13 +2,11 @@
 using namespace std;
 
 int main(){
-  int n, product, temp; //n factorial
+  int n, product; //n factorial
   cin >> n;
   product = 1;
-  temp = n;
-  for(int i = 0; i < n; i++){
-    product = product * temp;
-    temp -= 1;
+  for(int i = n; i > 0; i--){
+    product = product * i;
   }
   cout << product;
 }
